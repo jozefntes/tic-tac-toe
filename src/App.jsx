9a7300a170e5bab0502b9bdd17d783b051022854
jozefntes <123389422+jozefntes@users.noreply.github.com>
2024-03-13 23:50:21 -0500

    Add ability to end game and have a winner

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const Square = ({ children, isSelected, updateBoard, index }) => {
 
   const handleClick = () => {
     updateBoard(index);
-  }
+  };
 
   return (
     <div onClick={handleClick} className={className}>
@@ -19,20 +19,66 @@ const Square = ({ children, isSelected, updateBoard, index }) => {
   );
 };
 
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
   console.log(board);
 
   const [turn, setTurn] = useState(TURNS.X);
 
+  // null == no winner, false == draw
+  const [winner, setWinner] = useState(null);
+
+  const checkWinner = (boardToCheck) => {
+    // loop through all the winner combos
+    // to see if X or O won
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo;
+      // Check that the positions are not null and that they are the same
+      if (
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a];
+      }
+    }
+    // no winnner yet
+    return null;
+  };
+
   const updateBoard = (index) => {
+    // don't update this position if it's already filled
+    // or there is already a winner
+    if (board[index] || winner) {
+      return;
+    }
+
+    // update board
     const newBoard = [...board];
     newBoard[index] = turn;
     setBoard(newBoard);
 
+    // update turn
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurn);
-  }
+
+    // check for winner
+    const newWinner = checkWinner(newBoard);
+    if (newWinner) {
+      setWinner(newWinner);
+    }
+  };
 
   return (
     <main className="board">
